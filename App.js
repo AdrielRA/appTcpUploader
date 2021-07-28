5d69a9339b14998d2e5c16b8270c1049393a877a
AdrielRA <36385005+AdrielRA@users.noreply.github.com>
2021-07-28 14:47:21 -0300

    :tada: #00001 Commit inicial

--- App.js
+++ App.js
@@ -1,21 +1,240 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  LogBox,
+  ActivityIndicator,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import WS from "react-native-websocket";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import SnackBar from "react-native-snackbar-component";
+
+LogBox.ignoreLogs(["Animated: `useNativeDriver`"]);
 
 export default function App() {
+  const PORT = 3000;
+  const wsRef = useRef(null);
+  const [base64, setBase64] = useState();
+  const [ipAddress, setIpAddress] = useState();
+  const [hasLibraryPermission, setHasLibraryPermission] = useState();
+  const [hasScannerPermission, setHasScannerPermission] = useState();
+  const [scanned, setScanned] = useState(false);
+  const [sending, setSending] = useState(false);
+  const [showSnack, setShowSnack] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasScannerPermission(status === "granted");
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        setHasLibraryPermission(status === "granted");
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!!base64) setSending(true);
+  }, [base64]);
+
+  const handleBarCodeScanned = ({ data }) => {
+    setScanned(true);
+    setIpAddress(data);
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+      base64: true,
+    });
+
+    if (!result.cancelled) {
+      setBase64(result.base64);
+    }
+  };
+
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+      base64: true,
+    });
+
+    if (!result.cancelled) {
+      setBase64(result.base64);
+    }
+  };
+
+  const sendImage = (client, image) => {
+    const message = ["screenshot", image].join(",");
+    console.log(
+      `Sending ${image.length} bytes in message ${message.length} bytes`
+    );
+    client.send(message, () => {
+      console.log("Done");
+      process.exit(0);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      {hasScannerPermission === undefined ||
+      hasLibraryPermission === undefined ? (
+        <ActivityIndicator color="#fff" size="large" />
+      ) : !hasScannerPermission || !hasScannerPermission ? (
+        <Text style={styles.txt}>
+          O app não tem as permissões necessárias para funcionar.
+        </Text>
+      ) : (
+        <>
+          {!ipAddress ? (
+            <BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+              style={[StyleSheet.absoluteFillObject, styles.scanner]}
+            >
+              <View style={styles.layerTop}>
+                <Text style={styles.qr}>Escaneie o QR CODE</Text>
+              </View>
+              <View style={styles.layerCenter}>
+                <View style={styles.layerLeft} />
+                <View style={styles.focused} />
+                <View style={styles.layerRight} />
+              </View>
+              <View style={styles.layerBottom} />
+            </BarCodeScanner>
+          ) : (
+            <>
+              {sending ? (
+                <View>
+                  <Text style={[styles.txt, { marginBottom: 15 }]}>
+                    Enviando...
+                  </Text>
+                  <ActivityIndicator color="#fff" size="large" />
+                </View>
+              ) : (
+                <>
+                  <TouchableOpacity onPress={pickImage} style={styles.btn}>
+                    <Text style={styles.txt}>Pegar da galeria</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={takePhoto} style={styles.btn}>
+                    <Text style={styles.txt}>Tirar foto</Text>
+                  </TouchableOpacity>
+                </>
+              )}
+
+              {base64 && (
+                <WS
+                  ref={wsRef}
+                  url={`ws://${ipAddress}:${PORT}/`}
+                  onOpen={(ws) => {
+                    console.log("Open!");
+                    setHasError(false);
+                    sendImage(ws, base64);
+                  }}
+                  onMessage={(msg) => {
+                    console.log({ msg });
+                  }}
+                  onError={(err) => {
+                    setHasError(true);
+                    console.log({ err });
+                  }}
+                  onClose={() => {
+                    setBase64(null);
+                    setShowSnack(true);
+                    console.log("closed");
+                    setSending(false);
+                  }}
+                />
+              )}
+            </>
+          )}
+        </>
+      )}
+      <SnackBar
+        visible={showSnack}
+        textMessage={
+          hasError ? "Algo deu errado..." : "Imagem enviada com sucesso!"
+        }
+        autoHidingTime={5000}
+        backgroundColor={hasError ? danger : primary}
+        accentColor="#fff"
+        actionHandler={() => {
+          setShowSnack(false);
+        }}
+        actionText="OK"
+      />
     </View>
   );
 }
+const primary = "#38f";
+const danger = "#f43";
+const opacity = "rgba(0, 0, 0, .75)";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#222",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  scanner: {
+    flex: 1,
+  },
+  layerTop: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: opacity,
+  },
+  layerCenter: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  layerLeft: {
+    flex: 2,
+    backgroundColor: opacity,
+  },
+  focused: {
+    flex: 10,
+    borderWidth: 1,
+    borderColor: "rgba(255,255,255,0.5)",
+  },
+  layerRight: {
+    flex: 2,
+    backgroundColor: opacity,
+  },
+  layerBottom: {
+    flex: 1,
+    backgroundColor: opacity,
+  },
+  qr: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "700",
+  },
+  txt: { color: "#fff", fontSize: 15 },
+  btn: {
+    backgroundColor: primary,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginVertical: 20,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
